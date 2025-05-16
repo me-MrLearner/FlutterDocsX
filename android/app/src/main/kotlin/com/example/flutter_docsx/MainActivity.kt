61b7a8793e3c9e77b623b42cc7e3d9b26e7aed17
me-MrLearner <mr.learnerspace@gmail.com>
2025-05-16 10:01:04 +0600

    inital commit

---
+++ android/app/src/main/kotlin/com/example/flutter_docsx/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_docsx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_docsx/LearnDart/learn_dart.dart';
+
+// void main() => runApp(
+//   DevicePreview(
+//     enabled: !kReleaseMode,
+//     builder: (context) => MyApp(), // Wrap your app
+//   ),
+// );
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const LearnDart(),
+    );
+  }
+}
